a6382efed55a1b071f76d5056292f4a4b2cffe2e
Desti Ratna Komala <destiratnakomala@gmail.com>
2024-03-27 08:55:51 +0700

    v1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ke the analysis of pyspark with 100 million rows data',
+    author='Desti Ratna Komala',
+    license='',
+)
